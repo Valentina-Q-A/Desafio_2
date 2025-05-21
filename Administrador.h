9f7ddc63e16d8011b188c1e7312564e803acfac0
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2025-05-21 15:18:57 -0500

    Implementación de funciones para carga de archivos y actualización de sintaxis

--- Administrador.h
+++ Administrador.h
@@ -16,10 +16,7 @@ private:
 
 public:
     Administrador(string documento = "-", short int exp = 0, float estrellas = 0.0);
-    ~Administrador();
-
-    void agregarAlojamiento(Alojamiento* a);
-    bool eliminarAlojamiento(const string& codigo);
+    ~Administrador(); // Declaración del destructor
     void mostrarAlojamientos() const;
 
     string getDocumento() const;
@@ -30,6 +27,11 @@ public:
     void setDocumento(string d);
     void setExp(short int e);
     void setEstrellas(float est);
+
+    static Administrador* cargarDesdeLinea(const string& linea);
+
+    void agregarAlojamiento(Alojamiento* a);
+    Alojamiento* getAlojamiento(int index) const;
 };
 
 #endif
