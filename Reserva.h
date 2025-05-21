8cf7fa8a984b901ac56d6bb76f719dd499b434f6
Juan Felipe Orozco Londoño <juan.orozcol1@udea.edu.co>
2025-05-21 11:51:57 -0500

    Corrección std

--- Reserva.h
+++ Reserva.h
@@ -4,40 +4,41 @@
 #include "Fecha.h"
 #include <string>
 #include <iostream>
+using namespace std;
 
 class Reserva {
 private:
-    std::string id;
+    string id;
     Fecha fechaInicio;
     unsigned int noches;
-    std::string idAlojamiento;
-    char docCliente;
-    std::string metodoPago;
+    string idAlojamiento;
+    string docCliente;
+    string metodoPago;
     Fecha fechaPago;
     double valor;
-    std::string comentario;
+    string comentario;
 
 public:
     Reserva();
-    Reserva(std::string id, Fecha fi, unsigned int noches,
-            std::string idAlojamiento, char docCliente,
-            std::string metodoPago, Fecha fechaPago,
-            double valor, std::string comentario);
+    Reserva(string id, Fecha fi, unsigned int noches,
+            string idAlojamiento, string docCliente,
+            string metodoPago, Fecha fechaPago,
+            double valor, string comentario);
 
-    std::string getId() const;
+    string getId() const;
     Fecha getFechaInicio() const;
     Fecha getFechaSalida() const;
-    char getDocCliente() const;
-    std::string getIdAlojamiento() const;
-    std::string getMetodoPago() const;
-    std::string getComentario() const;
+    string getDocCliente() const;
+    string getIdAlojamiento() const;
+    string getMetodoPago() const;
+    string getComentario() const;
     double getValor() const;
 
     bool fechaEnUso(Fecha inicio, unsigned int nochesNueva) const;
     void mostrarResumen() const;
 
-    static Reserva* cargarDesdeLinea(const std::string& linea);
-    std::string aLinea() const;
+    static Reserva* cargarDesdeLinea(const string& linea);
+    string aLinea() const;
 };
 
 #endif
